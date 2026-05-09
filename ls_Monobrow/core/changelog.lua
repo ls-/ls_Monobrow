0eca15140291e8dde5e27c8a1ada4be026703294
Val Voronov <i.lightspark@gmail.com>
2026-05-10 00:36:52 +0200

    📜

--- ls_Monobrow/core/changelog.lua
+++ ls_Monobrow/core/changelog.lua
@@ -6,7 +6,5 @@ local _G = getfenv(0)
 
 -- Mine
 addon.CHANGELOG = [[
-- Added 12.0.5 support.
-- Fixed an issue where 3rd party LSM textures wouldn't load correctly.
-- Updated Simplified Chinese translation. Translated by 7peanuts@GitHub.
+- Fixed a memory leak caused by constant updates of trade post's and endeavours' data.
 ]]
